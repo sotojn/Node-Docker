f1452aeb6bdfe96b71ee40d3bcd1810649290d73
sotojn <jsoto@terascope.com>
2023-09-05 13:02:49 -0700

    make index.js adjustment to 2.0

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 8080;
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', (req, res) => {
   console.log('You got a request from the following ip ---> ' + req.socket.remoteAddress);
-  res.send('Hola World!');
+  res.send('Hola World 2.0!');
 });
 
 
